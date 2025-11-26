b4022db42a89b4ece220654924a3a8ac2ad6862d
Tran Gia Than <trangiathandz2004@gmail.com>
2025-11-27 02:35:28 +0700

    fix: convert frontend from submodule to normal folder

---
+++ frontend/src/api/auth.js
@@ -0,0 +1,10 @@
+export const loginApi = async (email, password) => {
+  const res = await fetch("/api/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
+
+  const data = await res.json();
+  return data.token;
+};
